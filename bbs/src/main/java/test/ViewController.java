b90e0d4f41bfcc2080458942dad8d0726aa934fe
Rogan kim <taegyun0922@gmail.com>
2016-10-04 22:44:30 +0900

    add to thymeleaf
    try to import thymeleaf

--- bbs/src/main/java/test/ViewController.java
+++ bbs/src/main/java/test/ViewController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class ViewController {
+		
 	@RequestMapping("/test")
 	@ResponseBody
 	String test(){
